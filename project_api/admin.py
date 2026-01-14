89660772ce0ff872baa1c120b13fe8723bb75c75
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2026-01-14 20:11:50 +0600

    admin.py updated

--- project_api/admin.py
+++ project_api/admin.py
@@ -1,3 +1,98 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Project
+
+
+@admin.register(Project)
+class ProjectAdmin(admin.ModelAdmin):
+    # List page
+    list_display = (
+        "title",
+        "category",
+        "is_featured",
+        "status",
+        "order",
+        "created_at",
+        "updated_at",
+    )
+    list_filter = (
+        "category",
+        "is_featured",
+        "status",
+        "created_at",
+    )
+    search_fields = (
+        "title",
+        "short_description",
+        "description",
+    )
+    ordering = ("order", "-created_at")
+
+    # Detail page
+    prepopulated_fields = {"slug": ("title",)}
+    readonly_fields = ("created_at", "updated_at", "deleted_at")
+
+    fieldsets = (
+        (
+            "Basic Information",
+            {
+                "fields": (
+                    "title",
+                    "slug",
+                    "category",
+                    "short_description",
+                    "description",
+                    "cover_image",
+                )
+            },
+        ),
+        (
+            "UI Metrics",
+            {
+                "fields": (
+                    "deliveries_count",
+                    "countries_count",
+                    "on_time_rate",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+        (
+            "Visibility & Highlight",
+            {
+                "fields": (
+                    "is_featured",
+                    "status",
+                    "order",
+                )
+            },
+        ),
+        (
+            "System Fields",
+            {
+                "fields": (
+                    "is_active",
+                    "created_at",
+                    "updated_at",
+                    "deleted_at",
+                ),
+                "classes": ("collapse",),
+            },
+        ),
+    )
+
+    # Performance & UX
+    list_select_related = ("category",)
+    list_editable = ("order", "is_featured", "status")
+    save_on_top = True
+
+    # Soft delete safety
+    actions = ["soft_delete_selected", "restore_selected"]
+
+    def soft_delete_selected(self, request, queryset):
+        queryset.update(is_active=False)
+    soft_delete_selected.short_description = "Soft delete selected projects"
+
+    def restore_selected(self, request, queryset):
+        queryset.update(is_active=True, deleted_at=None)
+    restore_selected.short_description = "Restore selected projects"
--- project_api/models/project.py
+++ project_api/models/project.py
@@ -1,4 +1,4 @@
-# projects/models.py
+# projects/models/project.py
 from django.db import models
 from django.utils.text import slugify
 
