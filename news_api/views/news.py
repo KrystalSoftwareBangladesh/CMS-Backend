f509d9789c317be3e9d69c45aa43a43f288d202c
Rezaul Karim Shaon <rkshaon.ist@gmail.com>
2026-01-16 20:40:51 +0600

    done: issue#41 - feature news and only one news can be feature at a time

--- news_api/views/news.py
+++ news_api/views/news.py
@@ -1,5 +1,8 @@
 # news/views/news.py
 from rest_framework import viewsets, permissions, filters
+from rest_framework import status
+from rest_framework.response import Response
+from rest_framework.decorators import action
 from django_filters.rest_framework import DjangoFilterBackend
 
 from news_api.models import News
@@ -42,3 +45,20 @@ class NewsViewSet(viewsets.ModelViewSet):
 
     def perform_destroy(self, instance):
         instance.soft_delete()
+
+    @action(detail=False, methods=["get"], url_path="featured")
+    def featured(self, request):
+        featured_news = (
+            self.get_queryset()
+            .filter(is_featured=True)
+            .first()
+        )
+
+        if not featured_news:
+            return Response(
+                {"detail": "No featured news found."},
+                status=status.HTTP_404_NOT_FOUND,
+            )
+
+        serializer = self.get_serializer(featured_news)
+        return Response(serializer.data)
